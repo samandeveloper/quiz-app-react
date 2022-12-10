26c1e4eb0d286c6443701f6c700eceea4bdaae79
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-10 00:56:08 -0700

    Updates

---
+++ precache-manifest.ee9d7407edc2e76eefb07bfc867bc874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d30fda9aefb53648a086f3461fe07b3",
+    "url": "/quiz-app-react/index.html"
+  },
+  {
+    "revision": "16e09f68e5c68dc29758",
+    "url": "/quiz-app-react/static/css/main.50566d9f.chunk.css"
+  },
+  {
+    "revision": "ba51c4628ef647e8c5a3",
+    "url": "/quiz-app-react/static/js/2.19fa2f29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app-react/static/js/2.19fa2f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e09f68e5c68dc29758",
+    "url": "/quiz-app-react/static/js/main.d2ef7dd1.chunk.js"
+  },
+  {
+    "revision": "65553d1697939789704c",
+    "url": "/quiz-app-react/static/js/runtime-main.bbd9f051.js"
+  }
+]);
